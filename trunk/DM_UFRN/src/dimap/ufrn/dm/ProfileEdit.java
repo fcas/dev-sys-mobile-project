f31376ea75b69472f22230fce856424385328a4b
almeidadalai@gmail.com <almeidadalai@gmail.com@acc80070-0f6f-3d77-9dc0-4bfa301fe24f>
2013-09-16 19:40:54 +0000

    travei a tela quanto a rotação... coloquei a imagem do bomberman para o usuario... ajeitei mais algumas coisas de layout (posicionamento)...

--- trunk/DM_UFRN/src/dimap/ufrn/dm/ProfileEdit.java
+++ trunk/DM_UFRN/src/dimap/ufrn/dm/ProfileEdit.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileEdit extends Activity {
 
+	Usuario usuario;
+	Button feito;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,16 +22,54 @@ public class ProfileEdit extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.profile_edit, menu);
+		setTitle("UFRN ON TOUCH");
+		usuario = (Usuario) getIntent().getSerializableExtra("usuario");
+		
+		
+		
 		return true;
 	}
 	
 	//Botão voltar...
-	@Override
-	public void onBackPressed() {
-		Intent voltaIntent = new Intent();
-		voltaIntent.setClass(ProfileEdit.this, MainActivity.class);
-		startActivity(voltaIntent);
-		finish();
-	}
+		@Override
+		public void onBackPressed() {
+			Intent voltaIntent = new Intent();
+			voltaIntent.putExtra("usuario", usuario);
+			voltaIntent.setClass(ProfileEdit.this, MainActivity.class);
+			startActivity(voltaIntent);
+			finish();
+			
+			
+		}
+		
+		private void setButtons() {
+
+			
+			
+			feito = (Button) findViewById(R.id.button1);
+			feito.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View view) {
+					
+					Usuario usuario = new Usuario();
+					Intent mainIntent = new Intent();
+
+					EditText editText = (EditText) findViewById(R.id.editText1);
+					usuario.setNome(editText.getText().toString());
+					EditText editText2 = (EditText) findViewById(R.id.editText2);
+					usuario.setCurso(editText2.getText().toString());
+					
+					mainIntent.putExtra("usuario", usuario);
+					
+					mainIntent.setClass(ProfileEdit.this,
+							MainActivity.class);
+					startActivity(mainIntent);
+
+					finish();
+				}
+			});
+			
+		}
 
 }
